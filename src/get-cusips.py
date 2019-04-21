b27476e5d257263ed9a35c181dcbf0cfdc004581
Tyler Copple <j.tylercopple@gmail.com>
2019-04-21 19:49:04 -0400

    Should have committed a long time ago, basically an initial commit. I'm repicking up this project ...

---
+++ src/get-cusips.py
@@ -0,0 +1,46 @@
+import json
+import time
+import requests
+import jsonpickle
+import sys
+import logging
+import pandas
+import os
+from services.openfigi_service import OpenFigiService
+from helpers.file_helpers import FileHelpers
+from helpers.logging_helpers import LoggingHelper
+
+##ENTRY POINT
+if __name__ == '__main__':
+
+    LoggingHelper.setup()
+    log = logging.getLogger(__name__)
+
+    transactions_path = FileHelpers.CONFIG["PATH_TRANSACTIONS"]
+    figis_path = FileHelpers.CONFIG["PATH_FIGI_DETAILS"]
+    figis_missing_path = FileHelpers.CONFIG["PATH_FIGI_MISSING"]
+    FileHelpers.try_make_directory(figis_path)
+    FileHelpers.try_make_directory(figis_missing_path)
+    figis = [os.path.splitext(f)[0] for f in os.listdir(figis_path)]
+    figis_missing = [os.path.splitext(f)[0] for f in os.listdir(figis_missing_path)]
+
+    for csv_filepath in FileHelpers.files_in_directory_with_path(transactions_path):
+        log.info("Reading [{}]".format(csv_filepath))
+        frame = pandas.read_csv(csv_filepath, index_col=None)
+        cusips = frame["cusip"].unique()
+
+        for cusip in cusips:
+            if(cusip in figis or cusip in figis_missing):
+                continue
+
+            log.info("Fetching Figi data for [{}]".format(cusip))
+            response = OpenFigiService.get_figi(cusip)[0]
+            if("error" in response):
+                dat = jsonpickle.encode({}, unpicklable=False)
+                filepath = os.path.join(figis_missing_path, "{}.json".format(cusip))
+            else:
+                filepath = os.path.join(figis_path, "{}.json".format(cusip))
+                dat = jsonpickle.encode(response["data"][0], unpicklable=False)
+
+            with open(filepath, "w") as output:
+                output.write(json.dumps(json.loads(dat), indent=4))
