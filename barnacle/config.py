703efde065a56a205ed7cc0f5f1d592e5cc85ccb
Tyler Copple <j.tylercopple@gmail.com>
2020-01-04 20:27:33 -0500

    Updating calls in config and requirements.

--- barnacle/config.py
+++ barnacle/config.py
@@ -1,16 +1,36 @@
-class BarnacleConfig:
-    DIR_ROOT = "/Users/home/tyler/Documents/src/py/trakker/"
-    DIR_DATA = "/Users/home/tyler/Documents/src/py/trakker/data"
-    DIR_SRC = "/Users/home/tyler/Documents/src/py/trakker/src"
+import os
+from os.path import abspath, dirname, join, realpath
 
-    PATH_FILINGS = "data/010-filings"
-    PATH_FILINGS_CSVS = "data/020-csvs"
-    PATH_COMPANY_REPORTS = "data/030-filings"
-    PATH_COMPANY_HOLDINGS = "data/040-holdings"
-    PATH_TRANSACTIONS = "data/050-transactions"
+try:
+    from dotenv import load_dotenv
+    load_dotenv(verbose=True)
+except ImportError, Exception as e:
+    pass
+
+class BarnacleConfig:
+
+    ROOT_PATH = abspath(join(dirname(realpath(__file__)), ".."))
+
+    DIR_DATA = os.environ.get("BARNACLE_DATA") or join(ROOT_PATH, "data")
+    DIR_SRC = os.environ.get("BARNACLE_SRC") or join(ROOT_PATH, "src")
+
+    S3_BUCKET = os.environ.get("S3_BUCKET")
+    S3_AWS_ACCESS_KEY = os.environ.get("S3_AWS_ACCESS_KEY")
+    S3_AWS_SECRET_KEY = os.environ.get("S3_AWS_SECRET_KEY")
+
+    PATH_FILINGS = join(S3_BUCKET, "010-filings")
+    PATH_FILINGS_CSVS = join(S3_BUCKET, "020-csvs")
+    PATH_COMPANY_REPORTS = join(S3_BUCKET, "030-filings")
+    PATH_COMPANY_HOLDINGS = join(S3_BUCKET, "040-holdings")
+    PATH_TRANSACTIONS = join(S3_BUCKET, "050-transactions")
 
     PATH_FIGI_DETAILS = "000-figi"
     PATH_FIGI_MISSING = "000-figi-missing"
 
-    START_YEAR = 2014
-    END_YEAR = 2016
+    START_YEAR = os.environ.get("BARNACLE_STARTYR") or 2014
+    END_YEAR = os.environ.get("BARNACLE_ENDYR") or 2016
+
+    OPENFIGI_URL = "https://api.openfigi.com/v1/mapping"
+    OPENFIGI_APIKEY = "4a455c69-3894-4542-b1ad-4584f102b36b"
+
+    SEC_URL_BASE = "https://www.sec.gov/Archives/edgar/full-index"
